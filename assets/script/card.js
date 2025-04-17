ff3fc52d4af6f53e73cfed9f974745e894621bf1
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-04-17 14:39:03 +0100

    end

--- assets/script/card.js
+++ assets/script/card.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.slider-wrapper', {
+/* const swiper = new Swiper('.slider-wrapper', {
     loop: true,
     grabCursor: true,
     spaceBetween: 50,
@@ -27,4 +27,39 @@ const swiper = new Swiper('.slider-wrapper', {
             slidesPerview: 3
         }
     }
-  });
+  }); */
+
+
+
+const swiper = new Swiper('.slider-wrapper', {
+
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }, 
+
+  //Responsive pou les breakpoints
+  breakpoints: {
+    0: {
+      slidesPerview: 1
+    },
+    620: {
+      slidesPerview: 2
+    },
+    1024: {
+      slidesPerview: 3
+    },
+  }
+});
